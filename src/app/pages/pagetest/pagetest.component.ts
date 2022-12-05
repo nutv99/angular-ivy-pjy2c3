909afdd3f84a7a93004cb8851cd3569651ca6345
nutv99 <nutv99@gmail.com>
2022-12-05 18:21:13 +0700

    3 files modified

--- src/app/pages/pagetest/pagetest.component.ts
+++ src/app/pages/pagetest/pagetest.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 // templateUrl: './department.component.html',
 export class PagetestComponent implements OnInit {
-
+             
   constructor() { }
 
   ngOnInit() {
