90d406e81932b0547e25ba1796be424966df1121
nutv99 <nutv99@gmail.com>
2022-12-06 09:32:36 +0700

    1 file modified

--- src/app/pages/pagetest/pagetest.component.ts
+++ src/app/pages/pagetest/pagetest.component.ts
@@ -10,6 +10,32 @@ import {
   faAddressBook,
 } from '@fortawesome/free-solid-svg-icons';
 
+export interface form_department { 
+  id : number ;
+  shopID : string ;
+  departmentCode : string ;
+  code2 : number ;
+  departmentDesc : string ;
+  lang : string ;
+  imageName : string ;
+  faIcon : string ;
+category: [{ 
+  id : number ;
+  categorycode : string ;
+  lang : string ;
+  categoryDesc : string ;
+  imageName : string ;
+  grandParentCode : string ;
+  departmentID : number ;
+  TotalProduct : number ;
+
+}],
+grouptopic:[{ 
+  categorycode : number ;
+  category : string ;
+  topiccode : number ;
+  topicdesc : string ;
+}],}
 
 
 @Component({
@@ -20,12 +46,16 @@ import {
 
 // templateUrl: './department.component.html',
 export class PagetestComponent implements OnInit {
+
+//   
   faDatabaseIcon = faDatabase;
   faFileIcon = faFile;
   faPrintIcon = faPrint;
   faGlobeIcon = faGlobe;
   faCheckIcon = faCheck;
   faAddressBookIcon = faAddressBook;
+
+
 
   constructor() {}
 
