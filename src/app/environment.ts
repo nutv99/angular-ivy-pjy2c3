4f0c72350c7daccb3ab8bda615361be2b7cfc8c1
nutv99 <nutv99@gmail.com>
2022-12-07 15:20:30 +0700

    1 file modified

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiHost: 'https://lovetoshopmall.com/swagger/marlinshopWork2/',
-  apiUrl: 'https://lovetoshopmall.com/swagger/marlinshopWork2',
+  apiUrl: 'https://lovetoshopmall.com/dataservice/testproject/',
 };
 
 export const apiPath = {
