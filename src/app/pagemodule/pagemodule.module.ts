798bccfc9218347092a9d4d4eb186c242d67b267
nutv99 <nutv99@gmail.com>
2022-12-07 12:08:13 +0700

    2 files modified, 3 added

--- src/app/pagemodule/pagemodule.module.ts
+++ src/app/pagemodule/pagemodule.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons'; 
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {BoutselectComponent} from '../shared/component2/boutselect/boutselect.component' ;
+
 import {PagetestComponent }  from '../pages/pagetest/pagetest.component' ;
 
 @NgModule({
   imports: [
     CommonModule,FontAwesomeModule
   ],
-  declarations: [PagetestComponent],
+  declarations: [BoutselectComponent,PagetestComponent],
   exports:[PagetestComponent] 
 })
 
---
+++ src/app/shared/component2/boutselect/boutselect.component.ts
@@ -0,0 +1,143 @@
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Input,
+  Output,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-boutselect',
+  templateUrl: './boutselect.component.html',
+  styleUrls: ['./boutselect.component.css'],
+})
+export class BoutselectComponent implements OnInit {
+  @Input() apiPathInput: string;
+  @Input() ModelName: string;
+  @Input() HaveChild: string;
+
+  @Input() dataInit: any;
+  @Input() Title: string;
+
+  @Output() myListChange: EventEmitter<string> = new EventEmitter();
+  sData: any = [{}];
+  @Input() results: any = [{ ID: '2', departmentDesc: 'CCCCCCC' }];
+  myurl: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    if (this.dataInit === '') {
+      this.InitDataSelect();
+    } else {
+      this.results = this.dataInit;
+    }
+    //this.InitDataSelect();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const veganValue = changes['dataInit'];
+    console.log('Change ', changes);
+    //veganValue.isFirstChange
+    if (veganValue.firstChange === false) {
+      this.results = veganValue.currentValue;
+      return;
+    }
+    //alert(veganValue.currentValue);
+    console.log(veganValue.firstChange);
+    //this.results = this.dataInit;
+  }
+
+  async InitDataSelect() {
+    let apiPathA = 'th/' + this.ModelName + '/All/1';
+
+    //alert(apiPathA);
+    this.myurl =
+      'https://lovetoshopmall.com/swagger/marlinshopWork2/' + apiPathA;
+    // this.myurl =
+    //   'https://lovetoshopmall.com/swagger/marlinshopWork2/th/department/All/1';
+
+    await this.http.get<any>(this.myurl).subscribe((data) => {
+      // อ่านค่า result จาก JSON response ที่ส่งออกมา
+      console.table('Data9999 For Select List', data);
+      //alert(data);
+      this.dataInit = data;
+      this.results = this.dataInit;
+      //this.myListChange.emit(this.sData);
+      //this.AllRec = data.totalRec;
+      // this.results = data;
+    });
+  }
+
+  async setOutData(sParentID) {
+    //this.results = '';
+    // this.myurl =
+    //   'https://lovetoshopmall.com/swagger/marlinshopWork2/' + this.apiPathInput;
+    if (this.HaveChild !== 'y') {
+      return;
+    }
+    this.myurl =
+      'https://lovetoshopmall.com/swagger/marlinshopWork2/th/' +
+      this.ModelName +
+      '/WithChild/' +
+      sParentID.target.value;
+    //alert(this.myurl);
+
+    await this.http.get<any>(this.myurl).subscribe((data) => {
+      // อ่านค่า result จาก JSON response ที่ส่งออกมา
+      console.table('Data For Select List OutData', data);
+      this.sData = data;
+      this.myListChange.emit(this.sData);
+      //this.AllRec = data.totalRec;
+      // this.results = data;
+    });
+  }
+}
+
+/*
+How To Use 
+
+ที่ไฟล์ Parent เรียกใช้
+<app-out-select 
+ [apiPathInput]="'th/department/All/1'"  
+ (myListChange)="BShowNumBerChange($event)"
+></app-out-select>
+// myListChange คือ OutputEmitter ที่จะ พ่่นค่า (Inject) ออกไปให้ Parent
+
+ที่ไฟล์ Parent สร้าง function เพื่อรับค่า จาก emit
+ในที่นี้คือ 
+BShowNumBerChange($event) 
+// Function ที่ฝั่ง Parent
+ BShowNumBerChange(e: any) {
+    //alert(e);
+    console.log('On App Component  99999', JSON.stringify(e));
+    this.orderno = e;
+  }
+
+Case การใช้   
+ 1. ต้องการ Child ให้กำหนด Parameter -> ModelName = ชื่อ Table
+            [dataInit]="''"
+            [HaveChild]="'n'"
+            (myListChange)="setChildData($event)"
+     หรือ 
+      <app-out-select
+            [apiPathInput]="'th/department/All/1'"
+            [ModelName]="'department'"
+            [dataInit]="''"
+            [HaveChild]="'y'"
+            (myListChange)="setChildData($event)" // ไปสร้าง function setChildData ไว้ใน Parent Component
+          >
+      </app-out-select>       
+ 2. ไม่ต้องการ Child ให้กำหนด Parameter -> ModelName
+      <app-out-select
+            [apiPathInput]="'th/category/All/1'"
+            [dataInit]="dataInit"
+            [ModelName]="'category'"
+            [HaveChild]="'n'"
+          >
+      </app-out-select>
+
+*/
