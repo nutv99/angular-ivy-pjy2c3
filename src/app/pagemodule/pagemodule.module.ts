0bcedbe2a77dccb948a31afd079b9638214f8b7e
nutv99 <nutv99@gmail.com>
2022-12-21 15:48:54 +0700

    1 file modified

--- src/app/pagemodule/pagemodule.module.ts
+++ src/app/pagemodule/pagemodule.module.ts
@@ -7,8 +7,10 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {BoutselectComponent} from '../shared/component2/boutselect/boutselect.component' ;
 import {OutSelectSearchlocalComponent} from '../shared/component2/out-select-searchlocal/out-select-searchlocal.component' ;
-
 import {PagetestComponent }  from '../pages/pagetest/pagetest.component' ;
+
+
+
 
 @NgModule({
   imports: [
