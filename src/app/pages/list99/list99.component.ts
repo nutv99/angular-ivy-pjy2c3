87f94055ad958945d72b7b562de405acb22822b6
nutv99 <nutv99@gmail.com>
2022-12-27 13:55:04 +0700

    1 file modified, 1 added, 1 deleted

--- src/app/pages/list99/list99.component.ts
+++ src/app/pages/list99/list99.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list99',
   templateUrl: './list99.component.html',
-  styleUrls: ['./list99.component.css']
+  styleUrls: ['./list99.component.scss']
 })
 export class List99Component implements OnInit {
 
